ea66a6d1a71ab9e0f331950a77dbb46bf812f8e2
Evelyn <evelabohemia@gmail.com>
2023-01-04 20:17:14 -0300

    Subida primigenia pruebas de controlador

---
+++ Villamigos/Villamigos.Web/Controllers/PruebasController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Villamigos.Web.Controllers
+{
+    public class PruebasController : Controller
+    {
+        public ContentResult Index()
+        {
+            string Mensaje = "Bienvenido a Villamigos  " +
+                $"Hoy es {DateTime.Today.ToString()}";
+            return new ContentResult
+            {
+                Content = Mensaje
+            };
+        }
+
+        public ContentResult Html()
+        {
+            ContentResult resultado = new ContentResult
+            {
+                Content = "<h1>Bienvenido a Villamigos</h1>",
+                ContentType = "html"
+            };
+            return resultado;
+        }
+
+        public JsonResult DatosJson()
+        {
+            var datos = new { Id = 1, nombre = "Ana" };
+            return new JsonResult(datos);
+        }
+
+
+        public ContentResult Prohibido()
+        {
+            ContentResult resultado = new ContentResult
+            {
+                StatusCode = 403,
+            };
+            return resultado;
+        }
+
+    }
+
+
+}
