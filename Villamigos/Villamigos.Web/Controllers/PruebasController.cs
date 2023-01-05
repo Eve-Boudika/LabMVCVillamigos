5df3e7065a14f0e9f652fe1766c0c37f199a190d
Evelyn <evelabohemia@gmail.com>
2023-01-04 21:13:35 -0300

    sumo vistas, acciones y form HTML

--- Villamigos/Villamigos.Web/Controllers/PruebasController.cs
+++ Villamigos/Villamigos.Web/Controllers/PruebasController.cs
@@ -41,6 +41,53 @@ namespace Villamigos.Web.Controllers
             return resultado;
         }
 
+        public ContentResult MostrarCodigo(string id)
+        {
+            return new ContentResult
+            {
+                Content = $"Codigo recibido:  {id}"
+            };
+        }
+
+        public ContentResult ProcesarNombre (string nombre)
+        {
+            return new ContentResult
+            {
+                Content = $"Hola: {nombre}  tu nombre tiene " +
+                $"{nombre.Length}  letras"
+            };
+        }
+
+        public ContentResult PrecioNeto(decimal Precio, decimal descuento)
+        {
+            decimal montoDescuento = Precio * descuento / 100;
+            decimal neto = Precio - descuento;
+            return new ContentResult
+            {
+                Content = $"Bruto:  {Precio}  Descuento: {montoDescuento} " +
+                $" Neto: {neto}"
+            };
+        }
+
+        public ViewResult Bienvenida()
+        {
+            ViewBag.framework = AppContext.TargetFrameworkName;
+            ViewBag.Hoy = DateTime.Today;
+           
+            return View();
+        }
+
+        public ViewResult MasaCorporal(decimal? peso, decimal? altura)
+        {
+            if(peso != null && altura != null)
+            {
+                ViewBag.Indice = peso.Value /
+                    (altura.Value * altura.Value);
+            }
+            return View();
+        }
+
+
     }
 
 
